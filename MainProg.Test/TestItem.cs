4f72fef6b4d87306d7a931951cc2b2750cda7b2b
haimnewclass <haim@newclass.co.il>
2023-02-20 22:45:42 +0200

    Add project files.

---
+++ MainProg.Test/TestItem.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace MainProg.Test
+{
+    [TestFixture]
+    public class TestItem
+    {
+        Items items = new Items();
+        [SetUp]
+        public void Init()
+        {
+            // initialize before all Tests
+        }
+
+        [Test]
+        [Category ("UnitTest - Check Actions")]
+        [Order (2) ]
+        public void TestPlus()
+        {
+            //Items items = new Items();
+            int ret = items.Plus(5, 5);
+
+            // if ret == 10 Test Succeed
+            // else Fail
+            if (ret == 10)
+            {
+                Assert.AreEqual(10, ret, "The results is Good");
+            }
+            else
+            {
+                Assert.AreEqual(1, items.Minus(5, 5));
+            }
+        }
+
+
+        [Test]
+        [Order(1)]
+        public void TestMinus()
+        {
+            
+            int ret = items.Minus(5, 5);
+
+            // if ret == 10 Test Succeed
+            // else Fail
+
+            //Assert.AreEqual(0, ret);
+            Random rand = new Random();
+            Random rand1 = new Random();
+            rand1 = rand;
+            // when referance type use Same
+            // when value type use Equal
+            Assert.AreSame(rand, rand1,"Random");
+       
+        }
+
+        [Test ]
+        public void TestMinus3()
+        {
+
+            int ret = items.Minus(5, 5);
+
+            // if ret == 10 Test Succeed
+            // else Fail
+
+            Assert.That(ret, Is.InRange(-1,-1));        
+        }
+
+        [Test, Ignore("Have not finished yet")]
+        [Order(1)]        
+        
+        public void TestMinus2()
+        {+
+
+            int ret = items.Minus(5, 5);
+
+            // if ret == 10 Test Succeed
+            // else Fail
+
+            Assert.AreEqual(0, ret);
+        }
+    }
+
+    
+}
---
+++ MainProg/Items.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProg
+{
+    public class Items
+    {
+        public Items()
+        {
+            list = new Dictionary<string, Item>();
+        }
+        public Dictionary<string, Item> list { get; set; }
+        public void Load()
+        {
+            list.Add("a", new Item() { Id = 1, Name = "banana" });
+            list.Add("b", new Item() { Id = 2, Name = "apple" });
+            list.Add("c", new Item() { Id = 3, Name = "grape" });
+        }
+
+        public int Calc(string itemName,int count)
+        {
+            Item item = list[itemName];
+            return item.Price* count;
+        }
+
+        public int Plus(int a,int b)
+        {
+            return a + b ;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
